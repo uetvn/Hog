329eebfd740e0152011a3199e7e01198042b740d
heroh396 <heroh396@gmail.com>
2017-08-16 11:59:01 +0700

    Add dir bin: save old data

---
+++ cpp/bin/src/config.h
@@ -0,0 +1,34 @@
+/*
+ * Project name   :
+ * File name      : config.h
+ * Created date   : Thu 13 Jul 2017 10:31:50 AM +07
+ * Author         : Ngoc-Sinh Nguyen
+ * Last modified  : Thu 13 Jul 2017 10:31:50 AM +07
+ * Desc           :
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+/* print foramt*/
+#define VHDL_FORMAT  0
+#define ONLY_BASIC_CASE 0
+/* All of them are mult 1024 */
+
+#define SIN10_MULT_1024   178
+#define SIN30_MULT_1024   512
+#define SIN50_MULT_1024   784
+#define SIN70_MULT_1024   962
+#define SIN90_MULT_1024   1024
+
+#define COS10_MULT_1024   1008
+#define COS30_MULT_1024   887
+#define COS50_MULT_1024   658
+#define COS70_MULT_1024   350
+#define COS90_MULT_1024   0
+
+
+
+#endif /* ifndef CONFIG_H */
+
