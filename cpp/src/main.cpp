5c8dc6ee00f2491ca4ff9ab99b2ed8d1140620ff
heroh396 <heroh396@gmail.com>
2017-08-16 09:55:25 +0700

    First commit

---
+++ cpp/src/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Project name   :
+ * File name      : main.cpp
+ * Created date   : Wed 16 Aug 2017 09:27:01 AM ICT
+ * Author         : Huy-Hung Ho
+ * Last modified  : Wed 16 Aug 2017 09:27:01 AM ICT
+ * Desc           :
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello world!" << endl;
+
+	return 0;
+}
+
